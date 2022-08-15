7ef90f69b5a55bbbab0f42e301bc2545d2914295
Yasser Akbbach <akbbach.yasser@gmail.com>
2022-08-15 23:34:27 +0100

    setup application and log config

---
+++ src/main/kotlin/com/yasserakbbach/borutoserver/Application.kt
@@ -0,0 +1,17 @@
+package com.yasserakbbach.borutoserver
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello Yasser!")
+        }
+    }
+}
