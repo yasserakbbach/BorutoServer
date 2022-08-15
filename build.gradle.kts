2054db14bb2e16539586d067e729edecf54f4dac
Yasser Akbbach <akbbach.yasser@gmail.com>
2022-08-15 23:47:48 +0100

    moved dependency versioning to gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val ktorVersion: String by project
+val ktorNettyVersion: String by project
+val logbackVersion: String by project
+
 plugins {
     application
     kotlin("jvm") version "1.6.21"
@@ -20,9 +24,9 @@ dependencies {
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
 
-    implementation("io.ktor:ktor-server-core:2.0.3")
-    implementation("io.ktor:ktor-server-netty:2.0.3")
-    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorNettyVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
 }
 
 tasks.test {
